7cd6644145666dc22987c13e5d5f1dfc660482c2
SantiagoMatiz <santiagomatiz13@gmail.com>
2025-02-18 08:39:32 -0500

    primer push

---
+++ activities/main.js
@@ -0,0 +1,7 @@
+let num1 = 20;
+let num2 = 25;
+
+console.log("suma: ",num1+num2)
+console.log("resta: ",num1-num2)
+console.log("multiplicación: ",num1*num2)
+console.log("divición: ",num1/num2)
---
+++ primitives/main.js
@@ -0,0 +1,7 @@
+let _Chain ="My first Chain";
+let _Num = 1;
+let _Bool = true;
+let _Array =[];
+let _Null = null;
+
+console.log(_Chain,"\n",_Num,"\n",_Bool,"\n",_Null)
